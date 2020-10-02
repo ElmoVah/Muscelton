460be6180555d28118065838725198d1ccffc009
Ronttipossu <elmo.vahvaselka@hotmail.com>
2020-10-02 13:44:21 +0300

    First()

--- app/src/main/java/com/example/muscelton/First.java
+++ app/src/main/java/com/example/muscelton/First.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.Collections;
@@ -20,6 +21,8 @@ import java.util.Random;
  * A simple {@link Fragment} subclass.
  */
 public class First extends Fragment {
+
+    final Random random = new Random();
 
 
     public First() {
@@ -33,23 +36,22 @@ public class First extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
 
-        final Random random = new Random();
 
-        Button buttonGenerateWorkout = findViewById(R.id.buttonGeneroi);
+        Button buttonGenerateWorkout = getView().findViewById(R.id.buttonGeneroi);
         buttonGenerateWorkout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 List<String> workoutSheet = Global.getInstance().getWorkoutSheet(getDifficulty());
                 Collections.shuffle(workoutSheet);
-                ((TextView)findViewById(R.id.textViewHarjoitus1))
+                ((TextView)getView().findViewById(R.id.textViewHarjoitus1))
                         .setText(workoutSheet.get(0));
-                ((TextView)findViewById(R.id.textViewHarjoitus2))
+                ((TextView)getView().findViewById(R.id.textViewHarjoitus2))
                         .setText(workoutSheet.get(1));
-                ((TextView)findViewById(R.id.textViewHarjoitus3))
+                ((TextView)getView().findViewById(R.id.textViewHarjoitus3))
                         .setText(workoutSheet.get(2));
-                ((TextView)findViewById(R.id.textViewHarjoitus4))
+                ((TextView)getView().findViewById(R.id.textViewHarjoitus4))
                         .setText(workoutSheet.get(3));
-                ((TextView)findViewById(R.id.textViewHarjoitus5))
+                ((TextView)getView().findViewById(R.id.textViewHarjoitus5))
                         .setText(workoutSheet.get(4));
 
             }
@@ -61,54 +63,65 @@ public class First extends Fragment {
             public void onClick(View v) {
                 int i = random.nextInt(Global.getInstance().getWorkoutSheet(getDifficulty()).size());
                 List<String> workoutSheet = Global.getInstance().getWorkoutSheet(getDifficulty());
-                ((TextView)findViewById(R.id.textViewHarjoitus1))
+                ((TextView)getView().findViewById(R.id.textViewHarjoitus1))
                         .setText(workoutSheet.get(i));
             }
         });
 
-        ImageButton reroll2 = findViewById(R.id.imageButtonReroll2);
+        ImageButton reroll2 = getView().findViewById(R.id.imageButtonReroll2);
         reroll2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int i = random.nextInt(Global.getInstance().getWorkoutSheet(getDifficulty()).size());
                 List<String> workoutSheet = Global.getInstance().getWorkoutSheet(getDifficulty());
-                ((TextView)findViewById(R.id.textViewHarjoitus2))
+                ((TextView)getView().findViewById(R.id.textViewHarjoitus2))
                         .setText(workoutSheet.get(i));
             }
         });
 
-        ImageButton reroll3 = findViewById(R.id.imageButtonReroll3);
+        ImageButton reroll3 = getView().findViewById(R.id.imageButtonReroll3);
         reroll3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int i = random.nextInt(Global.getInstance().getWorkoutSheet(getDifficulty()).size());
                 List<String> workoutSheet = Global.getInstance().getWorkoutSheet(getDifficulty());
-                ((TextView)findViewById(R.id.textViewHarjoitus3))
+                ((TextView)getView().findViewById(R.id.textViewHarjoitus3))
                         .setText(workoutSheet.get(i));
             }
         });
 
-        ImageButton reroll4 = findViewById(R.id.imageButtonReroll4);
+        ImageButton reroll4 = getView().findViewById(R.id.imageButtonReroll4);
         reroll4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int i = random.nextInt(Global.getInstance().getWorkoutSheet(getDifficulty()).size());
                 List<String> workoutSheet = Global.getInstance().getWorkoutSheet(getDifficulty());
-                ((TextView)findViewById(R.id.textViewHarjoitus4))
+                ((TextView)getView().findViewById(R.id.textViewHarjoitus4))
                         .setText(workoutSheet.get(i));
             }
         });
 
-        ImageButton reroll5 = findViewById(R.id.imageButtonReroll5);
+        ImageButton reroll5 = getView().findViewById(R.id.imageButtonReroll5);
         reroll5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int i = random.nextInt(Global.getInstance().getWorkoutSheet(getDifficulty()).size());
                 List<String> workoutSheet = Global.getInstance().getWorkoutSheet(getDifficulty());
-                ((TextView)findViewById(R.id.textViewHarjoitus5))
+                ((TextView)getView().findViewById(R.id.textViewHarjoitus5))
                         .setText(workoutSheet.get(i));
             }
 
         });
+    }
+
+    public int getDifficulty(){
+        RadioGroup rg = getView().findViewById(R.id.radioGroupValitsin);
+        if(rg.getCheckedRadioButtonId() == R.id.radioButtonHelppo){
+            return 1;
+        } else if(rg.getCheckedRadioButtonId() == R.id.radioButtonNormaali){
+            return 2;
+        } else {
+            return 3;
+        }
     }
 }
